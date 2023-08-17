123a0c2af1c3c2e47f03e4e1cd097fd363d379f4
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2023-08-17 11:31:03 -0300

    css part 2

--- src/BookForm.jsx
+++ src/BookForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { Button } from './styles';
-import { Formulario } from './styles';
+import { Button, Formulario } from './styles';
 
 const BookForm = ({ onAdd }) => {
   const [title, setTitle] = useState('')
@@ -21,20 +20,11 @@ const BookForm = ({ onAdd }) => {
     <Formulario>
       <h2>Adicionar Livro</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Título:
-          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-        </label>
+        <input type="text" placeholder='Título' value={title} onChange={(e) => setTitle(e.target.value)} />
         <br />
-        <label>
-          Autor:
-          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        </label>
+        <input type="text" placeholder='Autor' value={author} onChange={(e) => setAuthor(e.target.value)} />
         <br />
-        <label>
-          Gênero:
-          <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
-        </label>
+        <input type="text" placeholder='Gênero' value={genre} onChange={(e) => setGenre(e.target.value)} />
         <br />
         <Button isOn={title && author && genre} type="submit">Adicionar</Button>
       </form>
--- src/styles.js
+++ src/styles.js
@@ -14,6 +14,7 @@ export const Tela = styled.div`
 export const Container = styled.div`
   background-color: #FFF;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   width: 944px;
   height: 609px;
@@ -50,30 +51,56 @@ export const Container = styled.div`
     font-style: normal;
     margin-top: 50%;
   }
-
-  /*POSIÇÃO FORMULARIO*/
-  .forPosicao{
-    margin-left: 150px;
-  }
 `
 
-/*H2 ADICIONAR LIVRO*/
+/*FORMULÁRIO*/
 export const Formulario = styled.div`
-h2{
+  margin-right: 50px;
+  margin-top: 10px;
+
+  /*H2 ADICIONAR LIVRO*/
+  h2{
     color: #5B5FCD;
     font-size: 24px;
     font-style: normal;
+    margin-bottom: 15px;
+    margin-left: 112px;
+    margin-top: 50px;
   }
+  form{
+    margin-bottom: 30px;
+  }
+  input{
+    padding-left: 15px;
+    color: #454545;
+    font-weight: 600;
+    font-size: 17px;
+    margin-bottom: 6px;
+    width: 416px;
+    height: 60px;
+    border: none;
+    background-color: #D5D6E6;
+    outline: none;
+  }
+  ::placeholder{
+    color: #9A9A9A;
+    font-size: 16px;
+  }  
 `
 
 /*BOTÃO ADICIONAR*/
 export const Button = styled.button`
-  background: ${props => props.isOn ? '#000000' : 'gray'};
-  border-radius: 10px;
-  width: 150px;
-  height: 35px;
-  color: white;
-  border: none;
-  font-size: 15px;
+  background: #fff;
+  border-radius: 26px;
+  border: 2px solid ${props => props.isOn ? '#5B5FCD' : '#9A9A9A'};
+  font-weight: ${props => props.isOn ? '600' : '400'};
+  margin-left: 80px;
+  margin-top: 15px;
+  width: 234px;
+  height: 58px;
+  color: ${props => props.isOn ? '#5B5FCD' : '#9A9A9A'};
+  font-size: 20px;
   cursor: pointer;
 `
+
+
